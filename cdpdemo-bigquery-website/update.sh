3384eca5674290094c68cd647cfdac55cf8d91f6
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-11 11:58:10 +0800

    Update update.sh

--- cdpdemo-bigquery-website/update.sh
+++ cdpdemo-bigquery-website/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ng build
-sudo rm -rf /var/wwww/html
+sudo rm -rf /var/www/html
 sudo mkdir /var/www/html
 sudo cp -r $HOME/cdpdemo-bigquery-website/cdpdemo-bigquery-website/dist/cdpdemo-bigquery-website/. /var/www/html
