bb697f74c1537f254159fa1ea9719a2792cb7a0b
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-12 23:29:46 +0800

    Update app.module.ts

--- cdpdemo-bigquery-website/src/app/app.module.ts
+++ cdpdemo-bigquery-website/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,6 +22,7 @@ import { CustomMaterialModule } from './core/custom-material-module.module';
     AppRoutingModule,
     HttpClientModule,
     CustomMaterialModule, 
+    BrowserAnimationsModule
     
   ],
   providers: [RestService],
