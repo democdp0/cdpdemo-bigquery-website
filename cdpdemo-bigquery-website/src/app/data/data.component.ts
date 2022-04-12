e38f0c96706d6061a18ff027b7ac1a25efb5cf4a
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-13 00:12:25 +0800

    Update data.component.ts

--- cdpdemo-bigquery-website/src/app/data/data.component.ts
+++ cdpdemo-bigquery-website/src/app/data/data.component.ts
@@ -43,7 +43,7 @@ import * as _ from 'lodash';
       ]),
       transition(":leave", [
         style({ opacity: 1 }),
-        animate("500ms", style({  opacity: 0 }))
+        animate("0ms", style({  opacity: 0 }))
       ])
     ]),
     trigger("slideIn", [
