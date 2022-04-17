baca1f9e6b5f5e599956b43aadb6e95d9617e5c5
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-17 18:18:40 +0800

    Update goldenrecord.component.ts

--- cdpdemo-bigquery-website/src/app/goldenrecord/goldenrecord.component.ts
+++ cdpdemo-bigquery-website/src/app/goldenrecord/goldenrecord.component.ts
@@ -35,35 +35,6 @@ export class GoldenrecordComponent implements OnInit {
     script.type = `text/javascript`;
     script.text = `
         {
-          var viz;
-
-          function draw() {
-            var config = {
-              container_id: "viz",
-              server_url: "bolt://34.143.223.200/:7687",
-              server_user: "neo4j",
-              server_password: "dt",
-              labels: {
-                //"Character": "name",
-                "Character": {
-                  "caption": "name",
-                  "size": "pagerank",
-                  "community": "community"
-                  //"sizeCypher": "MATCH (n) WHERE id(n) = {id} MATCH (n)-[r]-() RETURN sum(r.weight) AS c"
-                }
-              },
-              relationships: {
-                "INTERACTS": {
-                  "thickness": "weight",
-                  "caption": false
-                }
-              },
-              initial_cypher: "MATCH (n:Person)-[r]-(d) where n.email = 'colin5905@hotmail.com' RETURN n,r,d"
-            };
-      
-            viz = new NeoVis.default(config);
-            viz.render();
-            console.log(viz);
 
         }
     `;
