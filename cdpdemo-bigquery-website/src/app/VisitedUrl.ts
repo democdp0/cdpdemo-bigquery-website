1bca2f97c74921843004aaf57c66011f65b892b6
Nur Akmal <2183322C@student.tp.edu.sg>
2022-04-19 00:11:41 +0800

    Update VisitedUrl.ts

--- cdpdemo-bigquery-website/src/app/VisitedUrl.ts
+++ cdpdemo-bigquery-website/src/app/VisitedUrl.ts
@@ -2,14 +2,14 @@ export class VisitedUrl
 {
 
     Email:string;
-    VisitedUrl:string;
+    url_visited:string;
 
 
     VisitedUrlCol : string[] = ["Email","url_visited" ];
-    constructor(email: string, VisitedUrl: string)
+    constructor(email: string, url_visited: string)
     {
         this.Email = email;
-        this.VisitedUrl = VisitedUrl;
+        this.url_visited = url_visited;
  
     }
 
